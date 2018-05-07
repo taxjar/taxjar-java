527461a51f81330bd9dcc7c5efef5fee89835dff
Jake Johnson <jake@fastdivision.com>
2018-05-07 10:48:21 -0700

    Version 1.1.0

--- src/main/java/com/taxjar/Taxjar.java
+++ src/main/java/com/taxjar/Taxjar.java
@@ -35,7 +35,7 @@ public class Taxjar {
     public static final String DEFAULT_API_URL = "https://api.taxjar.com";
     public static final String SANDBOX_API_URL = "https://api.sandbox.taxjar.com";
     public static final String API_VERSION = "v2";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
     protected static Endpoints apiService;
     protected static String apiUrl;
     protected static String apiToken;
