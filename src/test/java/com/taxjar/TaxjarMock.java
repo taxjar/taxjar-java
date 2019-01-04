9847c95d3ea0a4becb09913f23546183e93062d9
Jake Johnson <jake@fastdivision.com>
2019-01-03 18:42:23 -0800

    Mock timeouts and async exceptions

--- src/test/java/com/taxjar/TaxjarMock.java
+++ src/test/java/com/taxjar/TaxjarMock.java
@@ -10,6 +10,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public final class TaxjarMock extends Taxjar {
     public TaxjarMock(final String apiToken, Interceptor interceptor) {
@@ -23,7 +25,51 @@ public final class TaxjarMock extends Taxjar {
                         .build();
                 return chain.proceed(newRequest);
             }
-        }).addInterceptor(interceptor).build();
+        }).addInterceptor(interceptor)
+                .connectTimeout(timeout, TimeUnit.MILLISECONDS)
+                .writeTimeout(timeout, TimeUnit.MILLISECONDS)
+                .readTimeout(timeout, TimeUnit.MILLISECONDS)
+                .build();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(DEFAULT_API_URL + "/" + API_VERSION + "/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+        apiService = retrofit.create(Endpoints.class);
+    }
+
+    public TaxjarMock(final String apiToken, Map<String, Object> params, Interceptor interceptor) {
+        super(apiToken, params);
+
+        if (params != null) {
+            for (Map.Entry<String, Object> param : params.entrySet()) {
+                try {
+                    getClass().getDeclaredField(param.getKey()).set(this, param.getValue());
+                } catch (NoSuchFieldException | IllegalAccessException ex) {
+                    // No-op
+                }
+            }
+        }
+
+        final OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + apiToken)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).addInterceptor(interceptor)
+                .connectTimeout(timeout, TimeUnit.MILLISECONDS)
+                .writeTimeout(timeout, TimeUnit.MILLISECONDS)
+                .readTimeout(timeout, TimeUnit.MILLISECONDS)
+                .build();
 
         Gson gson = new GsonBuilder()
                 .setLenient()
--- src/test/java/com/taxjar/exception/ExceptionTest.java
+++ src/test/java/com/taxjar/exception/ExceptionTest.java
@@ -1,25 +1,27 @@
 package com.taxjar.exception;
 
 import com.taxjar.MockInterceptor;
+import com.taxjar.Taxjar;
 import com.taxjar.TaxjarMock;
 import com.taxjar.model.categories.CategoryResponse;
+import com.taxjar.net.Listener;
 import junit.framework.TestCase;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.MockResponse;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ExceptionTest extends TestCase {
-    private TaxjarMock client;
+    public void testException() {
+        TaxjarException e = null;
 
-    protected void setUp() {
         Map<String, Object> opts = new HashMap<>();
         opts.put("code", 400);
         MockInterceptor interceptor = new MockInterceptor("error.json", opts);
-        client = new TaxjarMock("TEST", interceptor);
-    }
-
-    public void testException() {
-        TaxjarException e = null;
+        TaxjarMock client = new TaxjarMock("TEST", interceptor);
 
         try {
             CategoryResponse res = client.categories();
@@ -30,5 +32,85 @@ public class ExceptionTest extends TestCase {
         assertTrue(e instanceof TaxjarException);
         assertEquals("Bad Request - Your request format is bad.", e.getMessage());
         assertEquals((Integer) 400, e.getStatusCode());
+    }
+
+    public void testExceptionAsync() throws InterruptedException {
+        Map<String, Object> opts = new HashMap<>();
+        opts.put("code", 400);
+        MockInterceptor interceptor = new MockInterceptor("error.json", opts);
+        TaxjarMock client = new TaxjarMock("TEST", interceptor);
+
+        client.categories(new Listener<CategoryResponse>() {
+            @Override
+            public void onSuccess(CategoryResponse res)
+            {
+                fail("Call was successful");
+            }
+
+            @Override
+            public void onError(TaxjarException e)
+            {
+                assertTrue(e instanceof TaxjarException);
+                assertEquals("Bad Request - Your request format is bad.", e.getMessage());
+                assertEquals((Integer) 400, e.getStatusCode());
+            }
+        });
+
+        Thread.sleep(1000);
+    }
+
+    public void testTimeoutException() throws IOException {
+        TaxjarException e = null;
+        MockWebServer server = new MockWebServer();
+
+        server.enqueue(new MockResponse().setBody("{}").setBodyDelay(800, TimeUnit.MILLISECONDS));
+        server.start();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("apiUrl", "http://" + server.getHostName() + ":" + server.getPort());
+        params.put("timeout", 500);
+        Taxjar client = new Taxjar("TEST", params);
+
+        try {
+            CategoryResponse res = client.categories();
+        } catch (TaxjarException ex) {
+            e = ex;
+        }
+
+        assertTrue(e instanceof TaxjarException);
+        assertEquals("timeout", e.getMessage());
+
+        server.shutdown();
+    }
+
+    public void testTimeoutExceptionAsync() throws IOException, InterruptedException {
+        MockWebServer server = new MockWebServer();
+
+        server.enqueue(new MockResponse().setBody("{}").setBodyDelay(800, TimeUnit.MILLISECONDS));
+        server.start();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("apiUrl", "http://" + server.getHostName() + ":" + server.getPort());
+        params.put("timeout", 500);
+        Taxjar client = new Taxjar("TEST", params);
+
+        client.categories(new Listener<CategoryResponse>() {
+            @Override
+            public void onSuccess(CategoryResponse res)
+            {
+                fail("Call was successful");
+            }
+
+            @Override
+            public void onError(TaxjarException e)
+            {
+                assertTrue(e instanceof TaxjarException);
+                assertEquals("timeout", e.getMessage());
+            }
+        });
+
+        Thread.sleep(1000);
+
+        server.shutdown();
     }
 }
